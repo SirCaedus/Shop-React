c1f21b1ca6356ac2c11f9d74df7015ee2f536104
SirCaedus <juanmartingonzalezmansilla@gmail.com>
2023-02-02 14:16:37 -0300

    agregando CartContainer

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -4,7 +4,7 @@ import Cart from '../assets/images/carrito.png'
 const CartWidget = () => {
     return(
         <Col  className='justify-content-end d-flex'>
-            <Nav.Link>
+            <Nav.Link as={NavLink} to='/cart'>
                 <span>0</span>
                 <img src={Cart} alt='imagen Carrito'/>
             </Nav.Link>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import ItemListContainer from './ItemListContainer'
 import ItemDetailContainer from './ItemDetailContainer'
+import CartContainer from './CartContainer'
 const Main = () => {
     return (
         <main>
@@ -8,6 +9,7 @@ const Main = () => {
                 <Route path='/' element={<ItemListContainer/>}/>
                 <Route path='/category/:categoria' element={<ItemListContainer/>}/>
                 <Route path='/item/:id' element={<ItemDetailContainer/>}/>
+                <Route path='/cart' element={<CartContainer/>}/>
                 <Route path='*' element={<div>404</div>}/>
             </Routes>
         </main>
